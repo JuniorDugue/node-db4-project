8c85a9b6965957b534dac03232603322b7f4fef0
juniordugue <jrdugue@gmail.com>
2020-02-21 12:39:55 -0500

    fixed bug on ingredients

--- receipes/model.js
+++ receipes/model.js
@@ -7,16 +7,33 @@ getInstructions(recipe_id): should return a list of step by step instructions fo
 module.exports = { 
     findRecipe, //get all of the receipes
     findShoppinglist, //returns list of all ingredients / quantities
-    findInstruction //returns list of instructions 
+    findInstruction, //returns list of instructions 
+    findIngredients,
+    findIngredientsId,
 }
 
 // recipesGET
-function findRecipe() {
-    return db('recipes')
-    .join('Recipes', 'Recipe_Book.recipe_id', 'Recipes.id')
-    .join('Ingredients', 'Recipe_Book.ingredient_id','Ingredients.id')
-    .select('name', 'quantity', 'ingredient', 'instructions');
- }
+function findRecipe(){
+    return db('Recipes')
+    .select('name')
+}
+
+// 
+function findIngredientsId(id){ 
+    return db('Ingredients')
+    .where({id})
+    .first()
+}
+
+function findIngredients(){ //to get everything
+    return db('Ingredients')
+}
+
+function findIngredients(){ 
+    return db('Ingredients')
+    .select('Receipe_Book.ingredient_id', 'Receipe_Book.quantity')
+    // .join('Receipe_Book', 'Receipe_Book.quantity', 'Ingredients.ingredient')
+}
 
 // shoppinglistGET
 function findShoppinglist(shopping_id) {
@@ -25,9 +42,17 @@ function findShoppinglist(shopping_id) {
     .select('recipe_id','quantity', 'ingredient')
     .where('recipe_id',recipe_id)
 }
+
 // instructionsGET
 function findInstruction(instruction_id) {
     return db('instructions')
     .select('instructions')
     .where({id})
 }
+
+// for ingredients
+// function findIngredients(){
+//     return db('ingredient')
+//     .select('Ingredients')
+//     .where({id})
+// }
--- receipes/router.js
+++ receipes/router.js
@@ -13,22 +13,45 @@ router.get('/', (req, res) => {
         })
 })
 
-  //Returns a list of the quantity and ingredients of a recipe /recipe/recipeIngredients/id
-  router.get('/ingredients/:id', (req, res) => {
-    const id = req.params.id;
-    data.findShoppinglist(id)
-        .then(shoppingList => {
-            if(!shoppingList) {
-                res.status(404).json({error: 'The specified ID does not exist.'})
-            } else {
-                res.status(200).json({shoppingList})
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({error: 'Could not get the list of Shopping Ingredients'})
-        })
+router.get('/ingredients', (req, res) => {
+    data.findIngredients()
+    .then(ingredients => {
+        res.status(200).json({ingredients})
+    })
+    .catch(err => {
+        res.status(500).json({error: 'could not get the list'})
+    })
 })
+
+// ingredients table
+router.get('/:id/ingredients', (req, res) => {
+    const id = req.params.id;
+
+    data.findIngredientsId(id)
+    .then(ingredients => {
+        res.status(200).json({ingredients})
+    }) 
+    .catch(err => {
+        res.status(500).json({error: 'no ingredients could be found'})
+    })
+})
+
+  //Returns a list of the quantity and ingredients of a recipe /recipe/recipeIngredients/id
+//   router.get('/ingredients/:id', (req, res) => {
+//     const id = req.params.id;
+//     data.findShoppinglist(id)
+//         .then(shoppingList => {
+//             if(!shoppingList) {
+//                 res.status(404).json({error: 'The specified ID does not exist.'})
+//             } else {
+//                 res.status(200).json({shoppingList})
+//             }
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             res.status(500).json({error: 'Could not get the list of Shopping Ingredients'})
+//         })
+// })
 
   //Returns a list of instructions of a recipe /recipe/instructions/id
   router.get('/instructions/:id', (req, res) => {
